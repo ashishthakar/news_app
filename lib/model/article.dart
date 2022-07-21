b54b626c9abac4914194b7bb0798bb8f0b5c89fd
THAKAR ASHISH <85748318+ashishthakar@users.noreply.github.com>
2022-07-21 19:45:27 +0530

    Initial commit

---
+++ lib/model/article.dart
@@ -0,0 +1,21 @@
+class ArticleModel{
+  final String author;
+  final String title;
+  final String description;
+  final String url;
+  final String img;
+  final String date;
+  final String content;
+
+  ArticleModel(this.author,this.title,this.description,this.url,this.img,this.date,this.content);
+  ArticleModel.fromjson(Map<String, dynamic>json)
+  : author = json["author"],
+    title = json["title"],
+    description = json["description"],
+    url = json["url"],
+    img = json["img"],
+    date = json["date"],
+    content = json["content"];
+
+  get source => null;
+}
